1f6aaec1427c114d755f53487a49f0e88eacf2a2
laps9802 <51708469+laps9802@users.noreply.github.com>
2019-11-27 16:07:03 +0900

    hide

--- app/src/main/java/com/cs/weather3/AppConstants.java
+++ app/src/main/java/com/cs/weather3/AppConstants.java
@@ -6,7 +6,7 @@ public class AppConstants {
     public static final String Naver_Base_URL = "https://naveropenapi.apigw.ntruss.com/";
 
     //API KEY
-    public static final String WEATHER_API_KEY = "y8AvfiQxzUU55zY8D1J0catYQprOeghBCVdShh7GE7qxDR7XrUTTiQTBm1N5RQLAh79aHvvkzJZf5pK1RruNug%3D%3D";    //need decoding
-    public static final String NAVER_MAP_API_ID = "4pqh9hkdar";
-    public static final String NAVER_MAP_API_KEY = "7OqqnVewqOjKWmBdNLo0pCOaqprbJViQ3ibU6Yze";
+    public static final String WEATHER_API_KEY = "******************************";    //need decoding
+    public static final String NAVER_MAP_API_ID = "******************************";
+    public static final String NAVER_MAP_API_KEY = "******************************";
 }
